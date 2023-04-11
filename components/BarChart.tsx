1b7bc03f30e2d7388af9e8b4707b14cb2062b681
Weten-Ogen <alphaoware@gmail.com>
2023-04-11 04:05:55 +0000

    completed the barchart

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react';
-// import {Bar} from 'react-chartjs-2';
+import {Bar} from 'react-chartjs-2';
 import {
     Chart as chartJs,
     CategoryScale,
@@ -28,37 +28,39 @@ const BarChart = () => {
   });
   const [chartOptions, setChartOptions] = useState({});
 
-  // useEffect(() =>{
-  //   setChartData({
-  //     labels:['Mon','Tue','Wed','Thurs','Fri','Sat','Sun'],
-  //     datasets:[
-  //       {label: 'Sales $',
-  //         data::[18127,22201,19490,17938,241842,22475],
-  //         borderColor: 'rgb(53,162,233)',
-  //         backgroundColor: 'rgb(53,162,233,0.4)'
-  //   }]}
-  // )
-  // setChartOptions({
-  //   plugins:{
-  //     legend:{
-  //       position: 'top',
-  //     },
-  //     title:{
-  //       display: true,
-  //       text: 'Daily Revenue',
-  //     }
-  //   },
-  //   maintainAspectRatio:false,
-  //   responsive: true
+  useEffect(() => {
+    setChartData({
+      labels:['Mon','Tues','Weds','Thurs','Fri','Sat',
+    'Sun'],
+    datasets:[{
+        label:'Sale $',
+        data:[18127,22201,19490, 17938,24182,17842,22475],
+        borderColor: 'rgb(53,162,235)',
+        backgroundColor: 'rgb(53,162,235,0.4',
+      }]
+    })
+    
+  setChartOptions({
+    plugins:{
+      legend:{
+        position: 'top',
+      },
+      title:{
+        display: true,
+        text: 'Daily Revenue',
+      }
+    },
+    maintainAspectRatio:false,
+    responsive: true
 
-  // })
+  })
       
-  // })
+},[])
 
   return (
     <>
       <section className='barchart'>
-        {/* <Bar data={chartData} options={chartOptions}/> */}
+        <Bar data={chartData} options={chartOptions}/>
       </section>
     </>
   )
