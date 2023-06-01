b9704cbc534e262154f57e19ffc637c6f3549088
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2023-06-01 02:40:11 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import '@/styles/globals.css';
-import Sidebar from '../components/Sidebar';
+import '../styles/globals.css';
+import Sidebar from '../components/SideBar';
 
 export default function App({ Component, pageProps }) {
   return (
