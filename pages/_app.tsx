12c6908ff5d71983c5a49f2c0aab9484510fffae
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2023-06-01 02:19:56 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,10 @@
-"use client"
+import '@/styles/globals.css';
+import Sidebar from '../components/Sidebar';
 
-import { SideBar } from '../components'
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import {useState,createContext} from 'react'
-
-export const AppContext= createContext()
-
-
-export default function App({ Component, pageProps }: AppProps) {
-  const [darkMode,setDarkMode] = useState(false);
-  const handleColorChange = () => {
-    setDarkMode(prev => !prev);
-  }
-  
+export default function App({ Component, pageProps }) {
   return (
-      <div className={darkMode? 'dark': ''}>
-        <AppContext.Provider value ={{darkMode,handleColorChange}}>
-          <SideBar >
-            <Component {...pageProps} />
-          </SideBar>
-        </AppContext.Provider>
-      </div>
-
-)}
+    <Sidebar>
+      <Component {...pageProps} />
+    </Sidebar>
+  );
+}
